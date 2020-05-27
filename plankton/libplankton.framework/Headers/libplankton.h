6537ff4fd6df71883686068176b3e393822cc186
Brandon Plank <52553007+brandonplank@users.noreply.github.com>
2020-05-26 23:14:45 -0500

    Re add

--- plankton/libplankton.framework/Headers/libplankton.h
+++ plankton/libplankton.framework/Headers/libplankton.h
@@ -86,18 +86,18 @@ void block_attribs(int fd, int block_now);
 int attribs(int fd, int baudrate);
 
 //Vars
-uint64_t offset_options;
-uint64_t offset_cr_flags;
-uint64_t offset_zonemap;
-uint64_t kernel_task_kaddr;
-uint64_t kernel_task_offset_all_image_info_addr;
-mach_port_t tfp0;
+uint64_t offset_options = 0;
+uint64_t offset_cr_flags = 0;
+uint64_t offset_zonemap=0;
+uint64_t kernel_task_kaddr=0;
+uint64_t kernel_task_offset_all_image_info_addr=0;
+mach_port_t tfp0=0;
 uint64_t kbase, kslide;
 thread_act_port_array_t thread_list;
 mach_msg_type_number_t thread_count;
 // get threads in task
 arm_thread_state64_t arm_state64;
-mach_msg_type_number_t sc;
+mach_msg_type_number_t sc = ARM_THREAD_STATE64_COUNT;
 
 
 
